7d74a4f7094b30b3141470a5782fafc448c02234
Ronal Galdamez <ronalgaldamez94@gmail.com>
2024-03-30 11:28:37 -0600

    Primer Commit.

---
+++ android/app/src/main/kotlin/com/example/togosv_01/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.togosv_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/src/styles/color.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/widgets.dart';
+
+const light = Color.fromARGB(255, 255, 255, 255);
